df8a00c533f5f32488c3a137f072d1d56bc40e42
Tobias Barth <mail@tobias-barth.net>
2025-06-11 16:57:36 +0200

    Create upload functionality

--- scripts/create-image-data.mjs
+++ scripts/create-image-data.mjs
@@ -1,22 +1,26 @@
 import { join as pJoin, basename } from "path";
 import { readdir, writeFile } from "fs/promises";
 import easyimage from "easyimage";
+import "dotenv/config";
 
 import { sequentialMapToPromiseAllWith } from "../utils/sequentialMapToPromiseAllWith.mjs";
 import { resizeToHeightAndWriteImageWith } from "../utils/resizeToHeightAndWriteImage.mjs";
 
 // CONFIGURATION
 
-const RAW_IMG_DIR = pJoin(import.meta.dirname, "..", "raw-imgs");
-const OUTPUT_DIR = "/public/images";
+const RAW_IMG_DIR = process.env.RAW_IMG_DIR;
+const rawImgDir = RAW_IMG_DIR.startsWith("/")
+  ? RAW_IMG_DIR
+  : pJoin(import.meta.dirname, "..", RAW_IMG_DIR);
+const OUTPUT_DIR = process.env.OUTPUT_DIR;
+const galleryDataPath = pJoin(
+  import.meta.dirname,
+  "../public",
+  process.env.GALLERY_DATA_PATH,
+);
+
 const LG_FILES_DIRNAME = "large";
 const SM_FILES_DIRNAME = "thumbnails";
-const GALLERY_CONFIG_PATH = pJoin(
-  import.meta.dirname,
-  "..",
-  "/public/data/grid-gallery-data.json",
-);
-const absoluteOutputDir = pJoin(import.meta.dirname, "..", OUTPUT_DIR);
 
 // END: CONFIGURATION
 
@@ -64,8 +68,8 @@ const fileInfoToGalleryInput =
     const filenameToSmPath = filenameToPath(pJoin(outputDir, SM_FILES_DIRNAME));
 
     return {
-      original: filenameToLgPath(lgInfoRecord.name).replace("public/", ""),
-      src: filenameToSmPath(smInfoRecord.name).replace("public/", ""),
+      original: filenameToLgPath(lgInfoRecord.name),
+      src: filenameToSmPath(smInfoRecord.name),
       width: smInfoRecord.width,
       height: smInfoRecord.height,
     };
@@ -87,8 +91,12 @@ const convertFilesWith =
     }
 
     // Create mappers
-    const filenameToLgPath = filenameToPath(pJoin(outputDir, LG_FILES_DIRNAME));
-    const filenameToSmPath = filenameToPath(pJoin(outputDir, SM_FILES_DIRNAME));
+    const filenameToLgPath = filenameToPath(
+      pJoin(import.meta.dirname, "../public", outputDir, LG_FILES_DIRNAME),
+    );
+    const filenameToSmPath = filenameToPath(
+      pJoin(import.meta.dirname, "../public", outputDir, SM_FILES_DIRNAME),
+    );
 
     // makeLgPathPair :: string -> [string, string]
     const makeLgPathPair = mapToList((x) => x, filenameToLgPath);
@@ -112,13 +120,12 @@ const convertFiles = convertFilesWith(easyimage);
 const makeImageInfoSuitableForGallery = (data) =>
   zip(data).map(fileInfoToGalleryInput(OUTPUT_DIR));
 const toJSON = (o) => JSON.stringify(o, null, 2);
-const writeToDataFile = (dataString) =>
-  writeFile(GALLERY_CONFIG_PATH, dataString);
+const writeToDataFile = (dataString) => writeFile(galleryDataPath, dataString);
 
 // Actual executing code
 
-getSourcePaths(RAW_IMG_DIR)
-  .then(convertFiles(absoluteOutputDir))
+getSourcePaths(rawImgDir)
+  .then(convertFiles(OUTPUT_DIR))
   .then(makeImageInfoSuitableForGallery)
   .then(toJSON)
   .then(writeToDataFile)
--- scripts/upload-images.mjs
+++ scripts/upload-images.mjs
@@ -1,11 +1,88 @@
-import { S3 } from "@aws-sdk/client-s3";
+import fs from "fs";
+import path from "path";
+import { S3, HeadBucketCommand } from "@aws-sdk/client-s3";
+import mime from "mime-types";
+import "dotenv/config";
 
 const client = new S3({
   forcePathStyle: false,
-  endpoint: "fra1.digitaloceanspaces.com",
+  endpoint: "https://fra1.digitaloceanspaces.com",
   region: "us-east-1",
   credentials: {
     accessKeyId: process.env.DO_SPACES_KEY,
     secretAccessKey: process.env.DO_SPACES_SECRET,
   },
 });
+
+const bucketExists = async (bucketName) => {
+  try {
+    await client.send(new HeadBucketCommand({ Bucket: bucketName }));
+    return true; // Bucket exists and is accessible
+  } catch (error) {
+    if (error.name === "NotFound") {
+      return false; // Bucket does not exist
+    }
+    if (error.name === "Forbidden") {
+      // Bucket exists, but you don't have permission
+      return true;
+    }
+    // Other unexpected errors
+    console.error("Error checking bucket:", error);
+    throw error;
+  }
+};
+
+const uploadFolderRecursively = async (
+  bucketName,
+  folderPath,
+  s3BaseKey = "",
+) => {
+  // withFileTypes is used to be able to call isFile and isDirectory on entries.
+  const entries = await fs.promises.readdir(folderPath, {
+    withFileTypes: true,
+  });
+  for (const entry of entries) {
+    const fullPath = path.join(folderPath, entry.name);
+    const s3Key = path.posix.join(s3BaseKey, entry.name);
+
+    if (entry.isDirectory()) {
+      await uploadFolderRecursively(bucketName, fullPath, s3Key);
+    } else if (entry.isFile()) {
+      const fileContent = await fs.promises.readFile(fullPath);
+      await client.putObject({
+        Bucket: bucketName,
+        Key: s3Key,
+        Body: fileContent,
+        ContentType: mime.lookup(entry.name) || "application/octet-stream",
+      });
+      console.log(`Uploaded ${s3Key}`);
+    }
+  }
+};
+
+if (!process.env.DO_SPACES_BUCKET_NAME) {
+  throw new Error("DO_SPACES_BUCKET_NAME environment variable is not set.");
+}
+
+/*
+ * Check if set bucket exists, if not, error out.
+ * If it exists, create appropriate directories and upload images.
+ */
+
+bucketExists(process.env.DO_SPACES_BUCKET_NAME)
+  .then((bucketExists) => {
+    if (!bucketExists) {
+      throw new Error(
+        `Bucket ${process.env.DO_SPACES_BUCKET_NAME} does not exist.`,
+      );
+    }
+  })
+  .then(() => {
+    console.log(
+      `Uploading images to bucket: ${process.env.DO_SPACES_BUCKET_NAME}`,
+    );
+    return uploadFolderRecursively(
+      process.env.DO_SPACES_BUCKET_NAME,
+      path.join(import.meta.dirname, "../public", process.env.OUTPUT_DIR),
+    );
+  });
