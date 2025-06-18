6e0e7690df6ee75cf2408419c9e5933859cff1e9
Tobias Barth <mail@tobias-barth.net>
2025-06-18 16:29:18 +0200

    Add lightbox

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { MasonryPhotoAlbum, Photo } from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
 import "react-photo-album/masonry.css";
-import { FunctionComponent } from "react";
+import "yet-another-react-lightbox/styles.css";
+import { FunctionComponent, useState } from "react";
 
 type GalleryProps = {
   photos: Array<Photo & { original: string }>;
@@ -13,12 +15,21 @@ export const Gallery: FunctionComponent<GalleryProps> = ({
   photos,
   className,
 }) => {
+  const [currentSlide, setCurrentSlide] = useState(-1);
   return (
-    <div className={className}>
-      <MasonryPhotoAlbum
-        photos={photos}
-        onClick={({ photo }) => console.log(photo.original)}
+    <>
+      <div className={className}>
+        <MasonryPhotoAlbum
+          photos={photos}
+          onClick={({ index }) => setCurrentSlide(index)}
+        />
+      </div>
+      <Lightbox
+        open={currentSlide >= 0}
+        index={currentSlide}
+        slides={photos.map((photo) => ({ src: photo.original }))}
+        close={() => setCurrentSlide(-1)}
       />
-    </div>
+    </>
   );
 };
